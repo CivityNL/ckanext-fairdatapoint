ccabd2adc3cd9d09973ac0766d2a9dcde4a2cabe
Hans-Chrstian <hanschristian.vanderwerf@health-ri.nl>
2023-12-20 10:51:18 +0100

    Merge BaseHarvester code into it

--- ckanext/fairdatapoint/harvesters/domain/fair_data_point_record_to_package_converter.py
+++ ckanext/fairdatapoint/harvesters/domain/fair_data_point_record_to_package_converter.py
@@ -1,8 +1,5 @@
 import logging
 
-from ckanext.civity.harvesters.domain.record_to_package_converter import \
-    IRecordToPackageConverter, \
-    RecordToPackageConverterException
 from ckanext.fairdatapoint.harvesters.domain.identifier import Identifier
 from ckanext.dcat.processors import RDFParser, RDFParserException
 from ckanext.fairdatapoint.processors import FairDataPointRDFParser
@@ -10,10 +7,9 @@ from ckanext.fairdatapoint.processors import FairDataPointRDFParser
 log = logging.getLogger(__name__)
 
 
-class FairDataPointRecordToPackageConverter(IRecordToPackageConverter):
+class FairDataPointRecordToPackageConverter():
 
     def __init__(self, profile):
-        IRecordToPackageConverter.__init__(self)
         self.profile = profile
 
     def record_to_package(self, guid, record):
@@ -30,4 +26,4 @@ class FairDataPointRecordToPackageConverter(IRecordToPackageConverter):
                 for dataset in parser.datasets():
                     return dataset
         except RDFParserException as e:
-            raise RecordToPackageConverterException('Error parsing the RDF content [{0}]: {1}'.format(record, e))
+            raise Exception('Error parsing the RDF content [{0}]: {1}'.format(record, e))
