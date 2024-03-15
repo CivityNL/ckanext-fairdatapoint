ef2dea8b9ba31ce5da241bb8950c44b35d3530d8
anastasianayden <anastasia.nayden@health-ri.nl>
2024-03-15 21:01:18 +0100

    fix: convert timestring to datetime; validate tags

--- ckanext/fairdatapoint/profiles.py
+++ ckanext/fairdatapoint/profiles.py
@@ -3,12 +3,20 @@
 #  check for multiple-text fields in the schema
 # All changes are © Stichting Health-RI and are licensed under the AGPLv3 license
 
+from datetime import datetime
+import re
+import json
+import logging
+
 from ckanext.dcat.profiles import EuropeanDCATAP2Profile
 from ckan.plugins import toolkit
 from ckan import model
-import json
-from typing import Dict
+import dateutil.parser as dateparser
+from dateutil.parser import ParserError
+from typing import Dict, List
 from rdflib import URIRef
+
+log = logging.getLogger(__name__)
 
 
 def _convert_extras_to_declared_schema_fields(dataset_dict: Dict) -> Dict:
@@ -31,17 +39,50 @@ def _convert_extras_to_declared_schema_fields(dataset_dict: Dict) -> Dict:
     # Populate the declared schema fields, if they are present in the extras
     for extra_dict in dataset_dict.get('extras', []):
         field_key = extra_dict.get('key')
+        field_value = extra_dict.get('value')
         if field_key in dataset_fields:
             preset = dataset_fields[field_key]
-            if preset == "multiple_text" and extra_dict.get('value'):
-                dataset_dict[field_key] = json.loads(extra_dict.get('value'))
+            if preset == 'multiple_text' and field_value:
+                dataset_dict[field_key] = json.loads(field_value)
+            elif preset == 'date' and field_value:
+                dataset_dict[field_key] = convert_datetime_string(field_value)
             else:
-                dataset_dict[field_key] = extra_dict.get('value')
+                dataset_dict[field_key] = field_value
 
     # Remove the extras that have been populated into the declared schema fields
     dataset_dict['extras'] = [d for d in dataset_dict['extras'] if d.get('key') not in dataset_fields]
 
     return dataset_dict
+
+
+def validate_tags(values_list: List[Dict]) -> List:
+    """
+    Validates tags strings to contain allowed characters, replaces others with spaces
+    """
+    illegal_pattern = re.compile('[^A-Za-z0-9\- _\.]')
+    tags = []
+    for item in values_list:
+        tag_value = item['name']
+        find_illegal = re.search(illegal_pattern, tag_value)
+        if find_illegal:
+            log.warning(f'Tag {tag_value} contains values other than alphanumeric characters, spaces, hyphens, '
+                        f'underscores or dots, they will be replaces with spaces')
+            tag = {'name': re.sub(illegal_pattern, ' ', tag_value)}
+            tags.append(tag)
+        else:
+            tags.append(item)
+    return tags
+
+
+def convert_datetime_string(date_value: str) -> datetime:
+    """
+    Converts datestrings (e.g. '2023-10-06T10:12:55.614000+00:00') to datetime class instance
+    """
+    try:
+        date_value = dateparser.parse(date_value)
+    except ParserError:
+        log.error('A date field string value can not be parsed to a date')
+    return date_value
 
 
 class FAIRDataPointDCATAPProfile(EuropeanDCATAP2Profile):
@@ -54,10 +95,12 @@ class FAIRDataPointDCATAPProfile(EuropeanDCATAP2Profile):
 
         dataset_dict = _convert_extras_to_declared_schema_fields(dataset_dict)
 
+        dataset_dict['tags'] = validate_tags(dataset_dict['tags'])
+
         # Example of adding a field
-        dataset_dict['extras'].append({'key': 'hello',
-                                       'value': "Hello from the FAIR data point profile. Use this function to do "
-                                                "FAIR data point specific stuff during the import stage"})
+        # dataset_dict['extras'].append({'key': 'hello',
+        #                                'value': 'Hello from the FAIR data point profile. Use this function to do '
+        #                                         'FAIR data point specific stuff during the import stage'})
 
         return dataset_dict
 
